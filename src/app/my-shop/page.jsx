f7e4c0847c5e2752ba4db478e6865d1c488fea14
Anisul Islam <anisulislam.pranto@gmail.com>
2024-11-08 20:18:21 +0600

    header

--- src/app/my-shop/page.jsx
+++ src/app/my-shop/page.jsx
@@ -22,33 +22,36 @@ export default function MyShopClient() {
                     <AddShopClient />
                 </div> 
                 : (user?.email && user?.shop) &&
-                <div className=' flex flex-col container mx-auto relative'>
-                    <div className=' relative h-56 sm:h-64 md:h-72 lg:h-96 w-full'>
-                        <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black"></span>
-                        <span className="absolute inset-0 w-full h-full bg-white border-2 border-black "></span>
-                        <Image className=' absolute object-cover' src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${user?.shop?.coverImage}`} alt='ShopImage' fill />
-                    </div>
-
-                    <div className=' ml-6 absolute bottom-20 md:bottom-28 flex flex-col md:gap-5 md:flex-row backdrop-blur-md text-white border border-white max-w-[80%] py-2'>
-                        <div className=' left-5 h-14 w-14 md:h-40 md:w-80 relative'>
-                            <Image className=' absolute object-contain' src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${user?.shop?.logo}`} alt='ShopImage' fill />
+                <>
+                    <div className=' flex flex-col container mx-auto relative'>
+                        <div className=' relative h-56 sm:h-64 md:h-72 lg:h-96 w-full'>
+                            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black"></span>
+                            <span className="absolute inset-0 w-full h-full bg-white border-2 border-black "></span>
+                            <Image className=' absolute object-cover' src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${user?.shop?.coverImage}`} alt='ShopImage' fill />
                         </div>
-                        <div className=' flex flex-col justify-center md:gap-2 pb-2 px-5 '>
-                            <h1 className=' text-lg sm:text-3xl md:text-3xl lg:text-5xl font-bold'>{user?.shop?.name}</h1>
-                            <p className='text-xs md:text-sm line-clamp-2'>{user?.shop?.description} Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi doloribus ab magni perspiciatis optio consequatur suscipit quo deleniti architecto nobis!</p>
-                        </div>
-                    </div>
 
-                    <div className=' flex justify-end items-end'>
-                        <div className=' flex items-end mt-5'>
-                            <AddProductClient />
+                        <div className=' ml-6 absolute bottom-20 md:bottom-28 flex flex-col md:gap-5 md:flex-row backdrop-blur-md text-white border border-white max-w-[80%] py-2'>
+                            <div className=' left-5 h-14 w-14 md:h-40 md:w-80 relative'>
+                                <Image className=' absolute object-contain' src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${user?.shop?.logo}`} alt='ShopImage' fill />
+                            </div>
+                            <div className=' flex flex-col justify-center md:gap-2 pb-2 px-5 '>
+                                <h1 className=' text-lg sm:text-3xl md:text-3xl lg:text-5xl font-bold'>{user?.shop?.name}</h1>
+                                <p className='text-xs md:text-sm line-clamp-2'>{user?.shop?.description} Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi doloribus ab magni perspiciatis optio consequatur suscipit quo deleniti architecto nobis!</p>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className=' absolute top-[19rem] sm:top-[22rem] md:top-[25rem] lg:top-[32rem]'>
+                        <div className=' flex justify-end items-end'>
+                            <div className=' flex items-end mt-5'>
+                                <AddProductClient />
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className=' flex flex-col container mx-auto '>
                         <ShopProductsClient products={user?.shop?.products} />
                     </div>
-                </div>
+                </>
+
             }
         </>
     )
--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -1,10 +1,39 @@
 'use client';
 
+import Image from 'next/image'
+import React, {useContext, useEffect, useState} from 'react'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RiCloseLargeLine } from "react-icons/ri";
+import Link from 'next/link';
+import logo from '../../assets/quick-basket.png'
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { fetchMe } from '@/lib/features/user/userSlice';
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+
+
+
+const navLinks = [
+    {
+        label: 'Home',
+        url: '/'
+    },
+    {
+        label: 'Shops',
+        url: '/shops'
+    },
+    {
+        label: 'Products',
+        url: '/products'
+    },
+    {
+        label: 'My Shop',
+        url: '/my-college'
+    },
+]
+
 
 export default function HeaderClient() {
+    const [open, setOpen] = useState(false);
     const { user } = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
@@ -14,7 +43,48 @@ export default function HeaderClient() {
         }
     }, [dispatch, user]);
 
+
     return (
-        <div>HeaderClient</div>
-    );
+        <>
+            <div className=' sticky top-0 z-50 bg-white shadow'>
+                    <div className=' flex items-center justify-between text-black lg:bg-transparent p-3 gap-5 relative md:px-24'>
+                        <Link href={'/'} className=' relative h-24 w-24'>
+                            <Image src={logo} alt='logo' fill className=' absolute object-cover' quality={50} />
+                        </Link>
+
+                        <div className=' hidden lg:flex items-center justify-between gap-5 font-poppinsRegular'>
+                            {
+                                navLinks.map((el) => 
+                                    <Link href={el.url} className='hover:text-gray-500 cursor-pointer ' key={el.url}>{el.label}</Link>
+                                )
+                            }
+
+                            <Link href={ user?.name ? '/profile' : '/signup'} className='hover:text-gray-500 cursor-pointer' >{user?.name ? user?.name : 'Sign up'}</Link>
+                        </div>
+
+                        {/* For Small Screen */}
+                        <button className=' block lg:hidden' onClick={() => setOpen(!open)} >
+                            {
+                                open ? <RiCloseLargeLine className=' w-8 h-8 text-black' /> : <RxHamburgerMenu className=' w-8 h-8 text-black' /> 
+                            }
+                        </button>
+                    </div>
+            </div>
+
+            {/* Dropdown For Small screen */}
+            <div 
+                className={`bg-white z-50 sticky top-24 w-full text-2xl gap-10 text-center flex flex-col font-poppinsRegular border-b ${open ? 'h-screen opacity-100 py-10' : 'h-0 opacity-0'} transition-all duration-300 ease-in-out overflow-hidden`}
+            >
+                {
+                    navLinks.map((el) => 
+                        <Link href={el.url} className='hover:text-gray-500 cursor-pointer ' key={el.url}>{el.label}</Link>
+                    )
+                }
+                <Link onClick={() => setOpen(false)} type='button' href={ user?.name ? '/profile' : '/signup'} className='hover:text-gray-500 cursor-pointer'>
+                    {user?.name ? user?.name : 'Sign up'}
+                </Link>
+            </div>
+
+        </>
+    )
 }
